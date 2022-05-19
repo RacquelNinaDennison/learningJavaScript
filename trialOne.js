08b898cfaa246626cfaafcac6ee2d07d6decbed6
RacquelNinaDennison <racquelnina@gmail.com>
2022-05-19 11:52:18 +0200

    Declaring Variables and Printing to the console

---
+++ trialOne.js
@@ -0,0 +1,19 @@
+// this is my first javascript project 
+//declaring variables
+// java scipt only runs in the browser and node acts as the interpter 
+// there are three methods , const let and var
+
+let a = 5; // let is content specific and sorta acts as a local variable
+// printing is console.log 
+
+console.log(a)
+
+// const is a variable is immutable
+const b = 'Racquel'
+console.log(b)
+
+// b =' Orange'
+// console.log(b)
+
+a = 'Pie'
+console.log(a)
